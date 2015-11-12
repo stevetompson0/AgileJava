159d2ecee420097b763e1cb5d2002f78f83da45f
steve xu <stevexucityu@gmail.com>
2015-11-11 23:20:52 -0800

    StudentUI test finished, input/output stream exercise

---
+++ main/java/com/model/StudentUI.java
@@ -0,0 +1,57 @@
+package main.java.com.model;
+
+import java.io.*;
+import java.util.*;
+
+public class StudentUI {
+	public final static String MENU = "(A)dd or (Q)uit?";
+	public final static String ADD_OPTION = "A";
+	public final static String QUIT_OPTION = "Q";
+	public final static String NAME_PROMPT = "Name: ";
+	public final static String ADDED_MESSAGE = "Added";
+	
+	private BufferedReader reader;
+	private BufferedWriter writer;
+	
+	private List<Student> students = new ArrayList<Student>();
+	
+	public StudentUI(BufferedReader reader, BufferedWriter writer){
+		this.reader = reader;
+		this.writer = writer;
+	}
+	
+	public List<Student> getAddedStudents(){
+		return students;
+	}
+	
+	public void run() throws IOException {
+		String line;
+		do {
+			write(StudentUI.MENU);
+			line = reader.readLine();
+			if (line.equals(StudentUI.ADD_OPTION))
+				addStudent();
+			
+		} while (!line.equals(StudentUI.QUIT_OPTION));
+	}
+	
+	private void addStudent() throws IOException{
+		write(StudentUI.NAME_PROMPT);
+		String name = reader.readLine();
+		students.add(new Student(name));
+		writeln(StudentUI.ADDED_MESSAGE);
+	}
+	
+	private void write(String line) throws IOException {
+		writer.write(line, 0, line.length());
+		writer.flush();
+	}
+	
+	private void writeln(String line) throws IOException {
+		writer.write(line);
+		writer.newLine();
+		writer.flush();
+	}
+	
+
+}
--- test/java/com/test/StudentUITest.java
+++ test/java/com/test/StudentUITest.java
@@ -1,13 +1,57 @@
 package test.java.com.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.*;
+import java.util.List;
+
 import org.junit.Test;
+
+import main.java.com.model.Student;
+import main.java.com.model.StudentUI;
 
 public class StudentUITest {
 	private static final String name = "Leo Xerces Schmoo";
 	
 	@Test
-	public void testCreateStudent(){
+	public void testCreateStudent() throws IOException{
+		StringBuilder expectedOutput = new StringBuilder();
+		StringBuilder input = new StringBuilder();
+		setup(expectedOutput, input);
+		byte[] buffer = input.toString().getBytes();
 		
+		InputStream inputStream = new ByteArrayInputStream(buffer);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		
+		OutputStream outputStream = new ByteArrayOutputStream();
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+		
+		StudentUI ui = new StudentUI(reader, writer);
+		ui.run();
+		
+		assertEquals(expectedOutput.toString(), outputStream.toString());
+		assertStudents(ui.getAddedStudents());
+		
+	}
+	
+	private String line(String input){
+		return String.format("%s%n", input);
+	}
+	
+	private void setup(StringBuilder expectedOutput, StringBuilder input){
+		expectedOutput.append(StudentUI.MENU);
+		input.append(line(StudentUI.ADD_OPTION));
+		expectedOutput.append(StudentUI.NAME_PROMPT);
+		input.append(line(name));
+		expectedOutput.append(line(StudentUI.ADDED_MESSAGE));
+		expectedOutput.append(StudentUI.MENU);
+		input.append(line(StudentUI.QUIT_OPTION));
+		
+	}
+	
+	private void assertStudents(List<Student> students){
+		assertEquals(students.size(), 1);
+		assertEquals(students.get(0).getName(), name);
 	}
 	
 
